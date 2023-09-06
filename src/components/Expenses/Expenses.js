cad068c4c6f353a12cde737fb79275ebeeab48dc
Shalmal Sanjay Sheth <shalmal.sheth@gmail.com>
2023-09-06 02:26:56 -0400

    using arrow functions

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css'
-import Card from './Card';
+import Card from '../UI/Card';
 
-function Expenses(){
+const Expenses = ()=>{
     const expenses = [
         {
           id: 'e1',
